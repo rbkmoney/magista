d86945feb39b91af1b28eda46de87b7c8f46517f
Vladimir Pankrashkin <v.pankrashkin@rbkmoney.com>
2016-08-03 14:53:19 +0300

    MST-7: Project base, minor DSL fixes

---
+++ src/main/java/com/rbkmoney/magista/MagistaApplication.java
@@ -0,0 +1,14 @@
+package com.rbkmoney.magista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication(scanBasePackages = {"com.rbkmoney.magista"})
+public class MagistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagistaApplication.class, args);
+	}
+}
