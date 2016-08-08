9799e86651ac6855c13e45d10859c80a172a1315
Pavel Popov <popoffpavel@yandex.ru>
2016-08-08 19:03:11 +0300

    MST-6: Add customer model

---
+++ src/main/java/com/rbkmoney/magista/model/Customer.java
@@ -0,0 +1,59 @@
+package com.rbkmoney.magista.model;
+
+import java.time.Instant;
+
+/**
+ * Created by tolkonepiu on 08.08.16.
+ */
+public class Customer {
+
+    private String id;
+
+    private String shopId;
+
+    private String merchantId;
+
+    private Instant createdAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(String shopId) {
+        this.shopId = shopId;
+    }
+
+    public String getMerchantId() {
+        return merchantId;
+    }
+
+    public void setMerchantId(String merchantId) {
+        this.merchantId = merchantId;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", shopId='" + shopId + '\'' +
+                ", merchantId='" + merchantId + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
