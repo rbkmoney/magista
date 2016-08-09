3120b887cc5a3353528bb4a0bb4b494d264536ac
Vladimir Pankrashkin <v.pankrashkin@rbkmoney.com>
2016-08-09 19:40:14 +0300

    MST-8: DSL Parser Impl

--- src/main/java/com/rbkmoney/magista/query/LimitedQueryResult.java
+++ src/main/java/com/rbkmoney/magista/query/LimitedQueryResult.java
@@ -3,6 +3,6 @@ package com.rbkmoney.magista.query;
 /**
  * Created by vpankrashkin on 03.08.16.
  */
-public abstract class LimitedQueryResult<T> extends QueryResult<T> {
+public abstract class LimitedQueryResult<T, RT> extends QueryResult<T, RT> {
     public abstract int getFullSize();
 }
