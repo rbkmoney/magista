993aa962a407429e4f70b8ade1a137b9aab8bc24
Vladimir Pankrashkin <v.pankrashkin@rbkmoney.com>
2016-08-30 13:18:01 +0300

    MST-17: review fixes: removed obsolete Pair class

--- src/main/java/com/rbkmoney/magista/dao/StatisticsDaoImpl.java
+++ src/main/java/com/rbkmoney/magista/dao/StatisticsDaoImpl.java
@@ -4,7 +4,6 @@ import com.rbkmoney.damsel.domain.InvoicePaymentStatus;
 import com.rbkmoney.magista.exception.DaoException;
 import com.rbkmoney.magista.model.Invoice;
 import com.rbkmoney.magista.model.Payment;
-import com.rbkmoney.magista.query.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.NestedRuntimeException;
