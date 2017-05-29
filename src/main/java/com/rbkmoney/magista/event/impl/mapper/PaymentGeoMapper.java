b0cd03d0a473fa834fc3d8a30c7ba140b205ea83
Pavel Popov <popoffpavel@yandex.ru>
2017-05-29 12:17:41 +0300

    MST-84: Add geo and commision mapper

--- src/main/java/com/rbkmoney/magista/event/impl/mapper/PaymentGeoMapper.java
+++ src/main/java/com/rbkmoney/magista/event/impl/mapper/PaymentGeoMapper.java
@@ -2,6 +2,7 @@ package com.rbkmoney.magista.event.impl.mapper;
 
 import com.rbkmoney.damsel.geo_ip.LocationInfo;
 import com.rbkmoney.damsel.geo_ip.geo_ipConstants;
+import com.rbkmoney.magista.domain.tables.pojos.InvoiceEventStat;
 import com.rbkmoney.magista.event.Mapper;
 import com.rbkmoney.magista.event.impl.context.InvoiceEventContext;
 import com.rbkmoney.magista.model.Payment;
@@ -28,21 +29,28 @@ public class PaymentGeoMapper implements Mapper<InvoiceEventContext> {
     @Override
     public InvoiceEventContext fill(InvoiceEventContext value) {
         Payment payment = value.getPayment();
+        InvoiceEventStat invoiceEventStat = value.getInvoiceEventStat();
 
         log.debug("Start geo enrichment, paymentId='{}', invoiceId='{}', eventId='{}', ip='{}'",
-                payment.getId(), payment.getInvoiceId(), payment.getEventId(), payment.getIp());
+                invoiceEventStat.getPaymentId(), invoiceEventStat.getInvoiceId(), invoiceEventStat.getEventId(), invoiceEventStat.getPaymentIp());
 
         try {
-            LocationInfo locationInfo = geoProvider.getLocationInfo(payment.getIp());
+            LocationInfo locationInfo = geoProvider.getLocationInfo(invoiceEventStat.getPaymentIp());
             payment.setCityId(locationInfo.getCityGeoId());
             payment.setCountryId(locationInfo.getCountryGeoId());
+
+            invoiceEventStat.setPaymentCityId(locationInfo.getCityGeoId());
+            invoiceEventStat.setPaymentCountryId(locationInfo.getCountryGeoId());
         } catch (ProviderException ex) {
-            log.warn("Failed to find city or country by ip:" + payment.getIp(), ex);
+            log.warn("Failed to find city or country by ip:" + invoiceEventStat.getPaymentIp(), ex);
             payment.setCityId(geo_ipConstants.GEO_ID_UNKNOWN);
             payment.setCountryId(geo_ipConstants.GEO_ID_UNKNOWN);
+
+            invoiceEventStat.setPaymentCityId(geo_ipConstants.GEO_ID_UNKNOWN);
+            invoiceEventStat.setPaymentCountryId(geo_ipConstants.GEO_ID_UNKNOWN);
         } finally {
             log.debug("End geo enrichment, paymentId='{}', invoiceId='{}', eventId='{}', ip='{}'",
-                    payment.getId(), payment.getInvoiceId(), payment.getEventId(), payment.getIp());
+                    invoiceEventStat.getPaymentId(), invoiceEventStat.getInvoiceId(), invoiceEventStat.getEventId(), invoiceEventStat.getPaymentIp());
         }
 
         value.setPayment(payment);
