5ecddeaa0c6b6ce64b3c3a456901a4805026e0f9
Pavel Popov <popoffpavel@yandex.ru>
2017-07-25 12:54:16 +0300

    MST-104: Fix migration script

--- src/main/resources/db/migration/V10__add_error_classification_support.sql
+++ src/main/resources/db/migration/V10__add_error_classification_support.sql
@@ -2,4 +2,5 @@ ALTER TABLE mst.invoice_event_stat RENAME payment_status_failure_code TO payment
 ALTER TABLE mst.invoice_event_stat RENAME payment_status_failure_description TO payment_external_failure_description;
 ALTER TABLE mst.invoice_event_stat ADD COLUMN payment_failure_class CHARACTER VARYING;
 
-UPDATE mst.invoice_event_stat SET payment_failure_class = 'operation_timeout' WHERE payment_external_failure_code ISNULL;
+UPDATE mst.invoice_event_stat SET payment_failure_class = 'operation_timeout' WHERE payment_external_failure_code ISNULL AND payment_status = 'failed'::mst.INVOICE_PAYMENT_STATUS;
+UPDATE mst.invoice_event_stat SET payment_failure_class = 'external_failure' WHERE payment_external_failure_code NOTNULL AND payment_status = 'failed'::mst.INVOICE_PAYMENT_STATUS;
