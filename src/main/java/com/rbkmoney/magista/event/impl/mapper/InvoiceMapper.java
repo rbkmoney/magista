715ec68262c495bf4939d95da59a2724e4b360a3
Pavel Popov <tolkonepiu@users.noreply.github.com>
2018-01-22 15:49:29 +0300

    Add checks for party revision fields (#113)

--- src/main/java/com/rbkmoney/magista/event/impl/mapper/InvoiceMapper.java
+++ src/main/java/com/rbkmoney/magista/event/impl/mapper/InvoiceMapper.java
@@ -75,7 +75,9 @@ public class InvoiceMapper implements Mapper<InvoiceEventContext> {
                 DamselUtil.getInvoiceStatusDetails(invoiceStatus)
         );
 
-        invoiceEventStat.setInvoicePartyRevision(invoice.getPartyRevision());
+        if (invoice.isSetPartyRevision()) {
+            invoiceEventStat.setInvoicePartyRevision(invoice.getPartyRevision());
+        }
 
         invoiceEventStat.setInvoiceCreatedAt(
                 TypeUtil.stringToLocalDateTime(invoice.getCreatedAt())
