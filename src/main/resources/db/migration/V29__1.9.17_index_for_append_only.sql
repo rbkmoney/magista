562d48bbd5453544e4dbfb44eb52f81c8d96cab9
Pavel Popov <popoffpavel@yandex.ru>
2018-02-14 18:24:29 +0300

    Rewrite indexes

--- src/main/resources/db/migration/V29__1.9.17_index_for_append_only.sql
+++ src/main/resources/db/migration/V29__1.9.17_index_for_append_only.sql
@@ -1,5 +1,8 @@
 DROP INDEX mst.invoice_stat_party_id_party_shop_id_payment_created_at_idx;
 DROP INDEX mst.invoice_stat_party_id_party_shop_id_invoice_created_at_idx;
 
-CREATE INDEX invoice_stat_party_id_party_shop_id_event_created_at_idx
-  ON mst.invoice_event_stat USING BTREE (id, party_id, party_shop_id, event_created_at);
+CREATE INDEX invoice_stat_event_category_event_created_at_idx
+  ON mst.invoice_event_stat USING BTREE (event_category, event_created_at);
+
+CREATE INDEX invoice_stat_event_category_party_id_shop_id_event_created_at_idx
+  ON mst.invoice_event_stat USING BTREE (event_category, party_id, party_shop_id, event_created_at);
