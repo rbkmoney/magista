ae9ab675a0fdea08344d7475b5196e6a4eb171a5
Pavel Popov <popoffpavel@yandex.ru>
2018-03-26 11:25:00 +0300

    Add tests

--- src/main/java/com/rbkmoney/magista/query/impl/parser/QueryParserImpl.java
+++ src/main/java/com/rbkmoney/magista/query/impl/parser/QueryParserImpl.java
@@ -20,6 +20,7 @@ public class QueryParserImpl extends BaseQueryParser {
                         new RootQuery.RootParser(),
                         new PaymentsFunction.PaymentsParser(),
                         new InvoicesFunction.InvoicesParser(),
+                        new RefundsFunction.RefundsParser(),
                         new PayoutsFunction.PayoutsParser(),
                         new CustomersRateStatFunction.CustomersRateStatParser(),
                         new PaymentsConversionStatFunction.PaymentsConversionStatParser(),
