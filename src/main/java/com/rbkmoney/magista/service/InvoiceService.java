568a8b506b9815b2216820a72599f97e7f0f8a14
Pavel Popov <popoffpavel@yandex.ru>
2018-10-09 11:53:53 +0300

    Change primary keys, cache boost

--- src/main/java/com/rbkmoney/magista/service/InvoiceService.java
+++ src/main/java/com/rbkmoney/magista/service/InvoiceService.java
@@ -1,5 +1,7 @@
 package com.rbkmoney.magista.service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.rbkmoney.magista.dao.InvoiceDao;
 import com.rbkmoney.magista.domain.tables.pojos.InvoiceData;
 import com.rbkmoney.magista.domain.tables.pojos.InvoiceEvent;
@@ -9,6 +11,7 @@ import com.rbkmoney.magista.exception.StorageException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,9 +23,31 @@ public class InvoiceService {
 
     private final InvoiceDao invoiceDao;
 
+    private final Cache<String, InvoiceData> invoiceDataCache;
+
     @Autowired
-    public InvoiceService(InvoiceDao invoiceDao) {
+    public InvoiceService(InvoiceDao invoiceDao, @Value("${cache.invoiceData.size}") int cacheSize) {
         this.invoiceDao = invoiceDao;
+        this.invoiceDataCache = Caffeine.newBuilder()
+                .maximumSize(cacheSize)
+                .build();
+    }
+
+    public InvoiceData getInvoiceData(String invoiceId) throws NotFoundException, StorageException {
+        return invoiceDataCache.get(
+                invoiceId,
+                key -> {
+                    try {
+                        InvoiceData invoiceData = invoiceDao.getInvoiceData(key);
+                        if (invoiceData == null) {
+                            throw new NotFoundException(String.format("Invoice data not found, invoiceId='%s'", key));
+                        }
+                        return invoiceData;
+                    } catch (DaoException ex) {
+                        throw new StorageException(String.format("Failed to get invoice data, invoiceId='%s'", key), ex);
+                    }
+                }
+        );
     }
 
     @Transactional(propagation = Propagation.REQUIRED)
@@ -30,6 +55,7 @@ public class InvoiceService {
         log.info("Trying to save invoice, invoiceData='{}', invoiceEvent='{}'", invoiceData, invoiceEvent);
         try {
             invoiceDao.saveInvoiceData(invoiceData);
+            invoiceDataCache.put(invoiceData.getInvoiceId(), invoiceData);
             invoiceDao.saveInvoiceEvent(invoiceEvent);
             log.info("Invoice have been saved, invoiceData='{}', invoiceEvent='{}'", invoiceData, invoiceEvent);
         } catch (DaoException ex) {
--- src/main/java/com/rbkmoney/magista/service/PaymentService.java
+++ src/main/java/com/rbkmoney/magista/service/PaymentService.java
@@ -1,6 +1,7 @@
 package com.rbkmoney.magista.service;
 
-import com.rbkmoney.magista.dao.InvoiceDao;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.rbkmoney.magista.dao.PaymentDao;
 import com.rbkmoney.magista.domain.tables.pojos.InvoiceData;
 import com.rbkmoney.magista.domain.tables.pojos.PaymentData;
@@ -12,46 +13,69 @@ import com.rbkmoney.magista.util.BeanUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.AbstractMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private final InvoiceDao invoiceDao;
+    private final InvoiceService invoiceService;
 
     private final PaymentDao paymentDao;
 
+    private final Cache<Map.Entry<String, String>, PaymentData> paymentDataCache;
+    private final Cache<Map.Entry<String, String>, PaymentEvent> paymentEventCache;
+
     @Autowired
-    public PaymentService(InvoiceDao invoiceDao, PaymentDao paymentDao) {
-        this.invoiceDao = invoiceDao;
+    public PaymentService(InvoiceService invoiceService, PaymentDao paymentDao, @Value("${cache.paymentData.size}") int cacheSize) {
+        this.invoiceService = invoiceService;
         this.paymentDao = paymentDao;
+        this.paymentDataCache = Caffeine.newBuilder()
+                .maximumSize(cacheSize)
+                .build();
+        this.paymentEventCache = Caffeine.newBuilder()
+                .maximumSize(cacheSize)
+                .build();
     }
 
-    public PaymentData getPaymentData(String invoiceId, String paymentId) throws StorageException {
-        try {
-            return paymentDao.getPaymentData(invoiceId, paymentId);
-        } catch (DaoException ex) {
-            throw new StorageException(String.format("Failed to get payment data, invoiceId='%s', paymentId='%s'", invoiceId, paymentId), ex);
-        }
+    public PaymentData getPaymentData(String invoiceId, String paymentId) throws NotFoundException, StorageException {
+        return paymentDataCache.get(
+                new AbstractMap.SimpleEntry<>(invoiceId, paymentId),
+                key -> {
+                    try {
+                        PaymentData paymentData = paymentDao.getPaymentData(key.getKey(), key.getValue());
+                        if (paymentData == null) {
+                            throw new NotFoundException(String.format("Payment data not found, invoiceId='%s', paymentId='%s'", key.getKey(), key.getValue()));
+                        }
+                        return paymentData;
+                    } catch (DaoException ex) {
+                        throw new StorageException(String.format("Failed to get payment data, invoiceId='%s', paymentId='%s'", key.getKey(), key.getValue()), ex);
+                    }
+                }
+        );
     }
 
     @Transactional(propagation = Propagation.REQUIRED)
     public void savePayment(PaymentData paymentData, PaymentEvent paymentEvent) throws NotFoundException, StorageException {
         log.info("Trying to save payment, paymentData='{}', paymentEvent='{}'", paymentData, paymentEvent);
         try {
-            InvoiceData invoiceData = invoiceDao.getInvoiceData(paymentData.getInvoiceId());
-            if (invoiceData == null) {
-                throw new NotFoundException(String.format("Invoice not found, invoiceId='%s'", paymentData.getInvoiceId()));
-            }
+            InvoiceData invoiceData = invoiceService.getInvoiceData(paymentData.getInvoiceId());
             paymentData.setPartyId(invoiceData.getPartyId());
             paymentData.setPartyShopId(invoiceData.getPartyShopId());
 
             paymentDao.savePaymentData(paymentData);
+            paymentDataCache.put(new AbstractMap.SimpleEntry<>(paymentData.getInvoiceId(), paymentData.getPaymentId()), paymentData);
+
             paymentDao.savePaymentEvent(paymentEvent);
+            paymentEventCache.put(new AbstractMap.SimpleEntry<>(paymentEvent.getInvoiceId(), paymentEvent.getPaymentId()), paymentEvent);
+
             log.info("Payment have been saved, paymentData='{}', paymentEvent='{}'", paymentData, paymentEvent);
         } catch (DaoException ex) {
             throw new StorageException(String.format("Failed to save payment, paymentData='%s', paymentEvent='%s'", paymentData, paymentEvent), ex);
@@ -61,16 +85,31 @@ public class PaymentService {
     public void savePaymentChange(PaymentEvent paymentEvent) throws NotFoundException, StorageException {
         log.info("Trying to save payment change, paymentEvent='{}'", paymentEvent);
         try {
-            PaymentEvent lastPaymentEvent = paymentDao.getPaymentEvent(paymentEvent.getInvoiceId(), paymentEvent.getPaymentId());
-            if (lastPaymentEvent == null) {
-                throw new NotFoundException(String.format("Payment changes not found, invoiceId='%s', paymentId='%s'", paymentEvent.getInvoiceId(), paymentEvent.getPaymentId()));
-            }
+            PaymentEvent lastPaymentEvent = getLastPaymentChange(paymentEvent.getInvoiceId(), paymentEvent.getPaymentId());
             BeanUtil.merge(lastPaymentEvent, paymentEvent, "id");
             paymentDao.savePaymentEvent(paymentEvent);
+            paymentEventCache.put(new AbstractMap.SimpleEntry<>(paymentEvent.getInvoiceId(), paymentEvent.getPaymentId()), paymentEvent);
             log.info("Payment change have been saved, paymentEvent='{}'", paymentEvent);
         } catch (DaoException ex) {
             throw new StorageException(String.format("Failed to save payment change, paymentEvent='%s'", paymentEvent), ex);
         }
+    }
+
+    private PaymentEvent getLastPaymentChange(String invoiceId, String paymentId) throws NotFoundException, StorageException {
+        return paymentEventCache.get(
+                new AbstractMap.SimpleEntry<>(invoiceId, paymentId),
+                key -> {
+                    try {
+                        PaymentEvent lastPaymentEvent = paymentDao.getLastPaymentEvent(key.getKey(), key.getValue());
+                        if (lastPaymentEvent == null) {
+                            throw new NotFoundException(String.format("Payment changes not found, invoiceId='%s', paymentId='%s'", key.getKey(), key.getValue()));
+                        }
+                        return lastPaymentEvent;
+                    } catch (DaoException ex) {
+                        throw new StorageException(String.format("Failed to get last payment change, invoiceId='%s', paymentId='%s'", key.getKey(), key.getValue()), ex);
+                    }
+                }
+        );
     }
 
 }
