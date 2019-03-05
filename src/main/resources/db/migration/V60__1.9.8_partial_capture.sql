0a13f2729ac4110cc4f2adece76a55a13e16099b
Pavel Popov <popoffpavel@yandex.ru>
2019-03-05 13:01:27 +0300

    Compress last migration

--- src/main/resources/db/migration/V60__1.9.8_partial_capture.sql
+++ src/main/resources/db/migration/V60__1.9.8_partial_capture.sql
@@ -3,16 +3,4 @@ ALTER TABLE mst.payment_event
 ALTER TABLE mst.payment_event
   ADD COLUMN payment_currency_code CHARACTER VARYING;
 
-UPDATE mst.payment_event
-SET payment_amount = payment_data.payment_amount,
-    payment_currency_code = payment_data.payment_currency_code
-FROM mst.payment_data
-WHERE payment_event.invoice_id = payment_data.invoice_id
-  AND payment_event.payment_id = payment_data.payment_id;
-
-ALTER TABLE mst.payment_event
-  ALTER COLUMN payment_amount SET NOT NULL;
-ALTER TABLE mst.payment_event
-  ALTER COLUMN payment_currency_code SET NOT NULL;
-
 ALTER TABLE mst.payment_data rename COLUMN payment_amount TO payment_origin_amount;
