b0c522ed2e54d2d5126bf1bdbeb7763e009b3eac
Pavel Popov <popoffpavel@yandex.ru>
2019-05-15 16:29:54 +0300

    Unify kafka properties

--- src/main/java/com/rbkmoney/magista/config/KafkaConfig.java
+++ src/main/java/com/rbkmoney/magista/config/KafkaConfig.java
@@ -1,6 +1,9 @@
 package com.rbkmoney.magista.config;
 
 import com.rbkmoney.machinegun.eventsink.MachineEvent;
+import com.rbkmoney.magista.config.properties.KafkaSslProperties;
+import com.rbkmoney.magista.log.KafkaErrorHandler;
+import com.rbkmoney.magista.retry.SimpleRetryPolicy;
 import com.rbkmoney.magista.serde.MachineEventDeserializer;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -8,6 +11,7 @@ import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -16,76 +20,98 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.ErrorHandler;
 import org.springframework.kafka.listener.LoggingErrorHandler;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableConfigurationProperties(KafkaSslProperties.class)
 public class KafkaConfig {
 
-    private static final String GROUP_ID = "MagistaListener";
-    private static final String EARLIEST = "earliest";
-    public static final String PKCS_12 = "PKCS12";
+    @Value("${kafka.consumer.auto-offset-reset}")
+    private String autoOffsetReset;
+    @Value("${kafka.consumer.enable-auto-commit}")
+    private boolean enableAutoCommit;
+    @Value("${kafka.consumer.group-id}")
+    private String groupId;
+    @Value("${kafka.client-id}")
+    private String clientId;
 
-    @Value("${kafka.bootstrap.servers}")
+    @Value("${kafka.bootstrap-servers}")
     private String bootstrapServers;
-    @Value("${kafka.concurrency}")
+    @Value("${kafka.consumer.concurrency}")
     private int concurrency;
-    @Value("${kafka.ssl.server-password}")
-    private String serverStorePassword;
-    @Value("${kafka.ssl.server-keystore-location}")
-    private String serverStoreCertPath;
-    @Value("${kafka.ssl.keystore-password}")
-    private String keyStorePassword;
-    @Value("${kafka.ssl.key-password}")
-    private String keyPassword;
-    @Value("${kafka.ssl.keystore-location}")
-    private String clientStoreCertPath;
-    @Value("${kafka.ssl.enable}")
-    private boolean kafkaSslEnable;
+
+    @Value("${retry-policy.maxAttempts}")
+    int maxAttempts;
 
     @Bean
-    public Map<String, Object> consumerConfigs() {
+    public Map<String, Object> consumerConfigs(KafkaSslProperties kafkaSslProperties) {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, MachineEventDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, EARLIEST);
-        if (kafkaSslEnable) {
-            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name());
-            props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, new File(serverStoreCertPath).getAbsolutePath());
-            props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, serverStorePassword);
-            props.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, PKCS_12);
-            props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, PKCS_12);
-            props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, new File(clientStoreCertPath).getAbsolutePath());
-            props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, keyStorePassword);
-            props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, keyPassword);
-        }
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommit);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+
+        configureSsl(props, kafkaSslProperties);
+
         return props;
     }
 
+    private void configureSsl(Map<String, Object> props, KafkaSslProperties kafkaSslProperties) {
+        if (kafkaSslProperties.isEnabled()) {
+            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name());
+            props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, new File(kafkaSslProperties.getTrustStoreLocation()).getAbsolutePath());
+            props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, kafkaSslProperties.getTrustStorePassword());
+            props.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, kafkaSslProperties.getKeyStoreType());
+            props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, kafkaSslProperties.getTrustStoreType());
+            props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, new File(kafkaSslProperties.getKeyStoreLocation()).getAbsolutePath());
+            props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, kafkaSslProperties.getKeyStorePassword());
+            props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, kafkaSslProperties.getKeyPassword());
+        }
+    }
+
     @Bean
-    public ConsumerFactory<String, MachineEvent> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    public ConsumerFactory<String, MachineEvent> consumerFactory(KafkaSslProperties kafkaSslProperties) {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(kafkaSslProperties));
     }
 
     @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, MachineEvent>> kafkaListenerContainerFactory(
             ConsumerFactory<String, MachineEvent> consumerFactory,
-            RetryTemplate retryTemplate
+            RetryTemplate kafkaRetryTemplate
     ) {
         ConcurrentKafkaListenerContainerFactory<String, MachineEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
         factory.getContainerProperties().setAckOnError(false);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
-        factory.setErrorHandler(new LoggingErrorHandler());
+        factory.setErrorHandler(kafkaErrorHandler());
         factory.setConcurrency(concurrency);
-        factory.setRetryTemplate(retryTemplate);
+        factory.setRetryTemplate(kafkaRetryTemplate);
         return factory;
+    }
+
+    @Bean
+    public RetryTemplate kafkaRetryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(
+                new SimpleRetryPolicy(maxAttempts, Collections.singletonMap(RuntimeException.class, true))
+        );
+        retryTemplate.setBackOffPolicy(new ExponentialBackOffPolicy());
+
+        return retryTemplate;
+    }
+
+    public ErrorHandler kafkaErrorHandler() {
+        return new KafkaErrorHandler();
     }
 }
