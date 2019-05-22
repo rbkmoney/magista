32caa0175c0f9c6fc251c058415096c70a2103e2
n.pospolita <nikita7asics@gmail.com>
2019-05-22 14:28:46 +0300

    added more tests

--- src/test/java/com/rbkmoney/magista/query/impl/search/EnrichedSearchQueryTest.java
+++ src/test/java/com/rbkmoney/magista/query/impl/search/EnrichedSearchQueryTest.java
@@ -1,15 +1,34 @@
 package com.rbkmoney.magista.query.impl.search;
 
+import com.rbkmoney.damsel.merch_stat.StatRequest;
+import com.rbkmoney.damsel.merch_stat.StatResponse;
 import com.rbkmoney.magista.query.AbstractQueryTest;
+import com.rbkmoney.magista.util.DamselUtil;
 import org.junit.Test;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
 
 @Transactional
 public class EnrichedSearchQueryTest extends AbstractQueryTest {
 
     @Test
-    public void test() {
-
+    @Sql("classpath:data/sql/search/enriched_invoices_search_data.sql")
+    public void testEnrichedPayments() {
+        String json = "{'query': {'enriched_payments': {'from_time': '2016-10-25T15:45:20Z','to_time': '3018-10-25T18:10:10Z'}}}";
+        StatResponse statResponse = queryProcessor.processQuery(new StatRequest(json));
+        assertEquals(2, statResponse.getData().getEnrichedInvoices().size());
+        DamselUtil.toJson(statResponse);
     }
 
+
+    @Test
+    @Sql("classpath:data/sql/search/enriched_invoices_search_data.sql")
+    public void testEnrichedRefunds() {
+        String json = "{'query': {'enriched_refunds': {}}}";
+        StatResponse statResponse = queryProcessor.processQuery(new StatRequest(json));
+        assertEquals(2, statResponse.getData().getEnrichedInvoices().size());
+        DamselUtil.toJson(statResponse);
+    }
 }
