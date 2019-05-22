a207499dc7f3f8963f68342775b8f0f3dadb2304
n.pospolita <nikita7asics@gmail.com>
2019-05-22 11:59:06 +0300

    ci fix

--- src/test/java/com/rbkmoney/magista/query/impl/search/EnrichedSearchQueryTest.java
+++ src/test/java/com/rbkmoney/magista/query/impl/search/EnrichedSearchQueryTest.java
@@ -1,10 +1,15 @@
 package com.rbkmoney.magista.query.impl.search;
 
 import com.rbkmoney.magista.query.AbstractQueryTest;
+import org.junit.Test;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public class EnrichedSearchQueryTest extends AbstractQueryTest {
 
+    @Test
+    public void test() {
+
+    }
 
 }
