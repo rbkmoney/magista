1745950fe3408644e7ef104778b685864c53643e
Pavel Popov <tolkonepiu@users.noreply.github.com>
2019-05-23 20:04:06 +0300

    Add optional location fetching (#201)

--- src/main/java/com/rbkmoney/magista/geo/GeoProviderImpl.java
+++ src/main/java/com/rbkmoney/magista/geo/GeoProviderImpl.java
@@ -4,16 +4,12 @@ import com.rbkmoney.damsel.geo_ip.GeoIpServiceSrv;
 import com.rbkmoney.damsel.geo_ip.LocationInfo;
 import com.rbkmoney.magista.provider.GeoProvider;
 import com.rbkmoney.magista.provider.ProviderException;
-import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
-/**
- * Created by alexeysemenkov on 08.08.16.
- */
 @Component
 public class GeoProviderImpl implements GeoProvider {
 
@@ -21,27 +17,18 @@ public class GeoProviderImpl implements GeoProvider {
 
     private final GeoIpServiceSrv.Iface columbusClient;
 
-    private final RetryTemplate retryTemplate;
-
     @Autowired
-    public GeoProviderImpl(GeoIpServiceSrv.Iface columbusClient, RetryTemplate retryTemplate) {
+    public GeoProviderImpl(GeoIpServiceSrv.Iface columbusClient) {
         this.columbusClient = columbusClient;
-        this.retryTemplate = retryTemplate;
     }
 
     @Override
     public LocationInfo getLocationInfo(String ip) throws ProviderException {
-        return retryTemplate.execute(context -> {
-                    if (context.getLastThrowable() != null) {
-                        log.warn("Failed to get location info (ip='{}'), retrying ({})...", ip, context.getRetryCount(), context.getLastThrowable());
-                    }
-
-                    try {
-                        return columbusClient.getLocation(ip);
-                    } catch (TException ex) {
-                        throw new ProviderException(ex);
-                    }
-                }
-        );
+        try {
+            return columbusClient.getLocation(ip);
+        } catch (Exception ex) {
+            log.warn("Failed to get location info, ip='{}'", ip, ex);
+            throw new ProviderException(ex);
+        }
     }
 }
