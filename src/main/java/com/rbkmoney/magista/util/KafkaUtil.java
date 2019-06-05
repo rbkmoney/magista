93d767d2f6f0b9c11fde82a3cd78b8d4599065bf
Pavel Popov <popoffpavel@yandex.ru>
2019-06-05 14:40:56 +0300

    Add consumer exception with batch summary

--- src/main/java/com/rbkmoney/magista/util/KafkaUtil.java
+++ src/main/java/com/rbkmoney/magista/util/KafkaUtil.java
@@ -19,17 +19,20 @@ public class KafkaUtil {
         ConsumerRecord lastRecord = records.get(records.size() - 1);
         LongSummaryStatistics keySizeSummary = records.stream().mapToLong(ConsumerRecord::serializedKeySize).summaryStatistics();
         LongSummaryStatistics valueSizeSummary = records.stream().mapToLong(ConsumerRecord::serializedValueSize).summaryStatistics();
-        String valueKeysString = records.stream().map(ConsumerRecord::value)
-                .map(value -> String.format("'%s.%d'", value.getSourceId(), value.getEventId()))
-                .collect(Collectors.joining(", "));
-        return String.format("topic='%s', partition=%d, offset={%d...%d}, createdAt={%s...%s}, keySize={min=%d, max=%d, avg=%s}, valueSize={min=%d, max=%d, avg=%s}, values={%s}",
+        return String.format("topic='%s', partition=%d, offset={%d...%d}, createdAt={%s...%s}, keySize={min=%d, max=%d, avg=%s}, valueSize={min=%d, max=%d, avg=%s}",
                 firstRecord.topic(), firstRecord.partition(),
                 firstRecord.offset(), lastRecord.offset(),
                 Instant.ofEpochMilli(firstRecord.timestamp()), Instant.ofEpochMilli(lastRecord.timestamp()),
                 keySizeSummary.getMin(), keySizeSummary.getMax(), keySizeSummary.getAverage(),
-                valueSizeSummary.getMin(), valueSizeSummary.getMax(), valueSizeSummary.getAverage(),
-                valueKeysString
+                valueSizeSummary.getMin(), valueSizeSummary.getMax(), valueSizeSummary.getAverage()
         );
+    }
+
+    public static String toSummaryStringWithValues(List<ConsumerRecord<String, MachineEvent>> records) {
+        String valueKeysString = records.stream().map(ConsumerRecord::value)
+                .map(value -> String.format("'%s.%d'", value.getSourceId(), value.getEventId()))
+                .collect(Collectors.joining(", "));
+        return String.format("%s, values={%s}", toSummaryString(records), valueKeysString);
     }
 
 }
