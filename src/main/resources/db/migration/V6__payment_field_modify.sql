ff0bed2ce8707141460f15b989adf3eac7fb45a6
vbanin <vitaxa93gamebox@gmal.com>
2019-07-08 13:14:48 +0300

    add index

--- src/main/resources/db/migration/V6__payment_field_modify.sql
+++ src/main/resources/db/migration/V6__payment_field_modify.sql
@@ -1,3 +1,6 @@
 ALTER TABLE mst.payment_data RENAME COLUMN payment_bank_card_bin TO payment_bank_card_first6;
 
 ALTER TABLE mst.payment_data RENAME COLUMN payment_bank_card_masked_pan TO payment_bank_card_last4;
+
+CREATE INDEX payment_data_payment_first6_idx ON payment_data (payment_bank_card_first6);
+CREATE INDEX payment_data_payment_last4_idx ON payment_data (payment_bank_card_last4);
