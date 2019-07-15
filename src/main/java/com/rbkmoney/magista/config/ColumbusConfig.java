4543622452b9f5fe4dc153807ccbba51b220c5ff
Pavel Popov <tolkonepiu@users.noreply.github.com>
2019-07-15 18:52:56 +0300

    Add rrn handler to component list (#219)

--- src/main/java/com/rbkmoney/magista/config/ColumbusConfig.java
+++ src/main/java/com/rbkmoney/magista/config/ColumbusConfig.java
@@ -1,10 +1,7 @@
 package com.rbkmoney.magista.config;
 
 import com.rbkmoney.damsel.geo_ip.GeoIpServiceSrv;
-import com.rbkmoney.woody.api.event.CompositeClientEventListener;
 import com.rbkmoney.woody.thrift.impl.http.THSpawnClientBuilder;
-import com.rbkmoney.woody.thrift.impl.http.event.ClientEventLogListener;
-import com.rbkmoney.woody.thrift.impl.http.event.HttpClientEventLogListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
