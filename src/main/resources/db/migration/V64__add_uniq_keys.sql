9bb493665aa216c2301a56014da96f29738a7b5d
Pavel Popov <popoffpavel@yandex.ru>
2019-07-19 16:58:37 +0300

    Fix migration script

--- src/main/resources/db/migration/V64__add_uniq_keys.sql
+++ src/main/resources/db/migration/V64__add_uniq_keys.sql
@@ -1,7 +1,7 @@
-drop index if exists refund_ukey;
-drop index if exists adjustment_ukey;
-drop index if exists invoice_event_ukey;
-drop index if exists payment_event_ukey;
+alter table mst.refund drop constraint refund_ukey;
+alter table mst.adjustment drop constraint adjustment_ukey;
+alter table mst.invoice_event drop constraint invoice_event_ukey;
+alter table mst.payment_event drop constraint payment_event_ukey;
 
 alter table mst.refund add constraint refund_new_ukey unique (invoice_id, payment_id, refund_id, event_id, event_type, refund_status);
 alter table mst.adjustment add constraint adjustment_new_ukey unique (invoice_id, payment_id, adjustment_id, event_id, event_type, adjustment_status);
