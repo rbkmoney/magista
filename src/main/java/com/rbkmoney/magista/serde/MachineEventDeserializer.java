3718629de014228423f0ebbf2c49871cea94e70d
Pavel Popov <popoffpavel@yandex.ru>
2019-07-23 23:37:58 +0300

    Fix package

--- src/main/java/com/rbkmoney/magista/serde/MachineEventDeserializer.java
+++ src/main/java/com/rbkmoney/magista/serde/MachineEventDeserializer.java
@@ -1,6 +1,6 @@
 package com.rbkmoney.magista.serde;
 
-import com.rbkmoney.damsel.withdrawals.processing.SinkEvent;
+import com.rbkmoney.machinegun.eventsink.SinkEvent;
 import com.rbkmoney.kafka.common.serialization.AbstractThriftDeserializer;
 
 public class MachineEventDeserializer extends AbstractThriftDeserializer<SinkEvent> {
