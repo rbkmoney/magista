e2346b42a3ecbf55ad9ae4b34bf6455cdbfa4b36
vitaxa <v.banin@rbkmoney.com>
2020-05-14 21:11:49 +0300

    BJ-892: chargeback dao fix

--- src/main/java/com/rbkmoney/magista/dao/impl/ChargebackDaoImpl.java
+++ src/main/java/com/rbkmoney/magista/dao/impl/ChargebackDaoImpl.java
@@ -6,6 +6,7 @@ import com.rbkmoney.magista.domain.tables.pojos.ChargebackData;
 import com.rbkmoney.magista.domain.tables.records.ChargebackDataRecord;
 import org.jooq.Query;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 
 import static com.rbkmoney.magista.domain.tables.ChargebackData.CHARGEBACK_DATA;
 
+@Component
 public class ChargebackDaoImpl extends AbstractDao implements ChargebackDao {
 
     private final RowMapper<ChargebackData> chargebackRowMapper;
