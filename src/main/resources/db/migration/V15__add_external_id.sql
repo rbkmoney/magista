78e861179a794f620e99d4c33e5f414a5979aada
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2020-05-26 17:48:10 +0300

    Ignore test, added indicies

--- src/main/resources/db/migration/V15__add_external_id.sql
+++ src/main/resources/db/migration/V15__add_external_id.sql
@@ -1,3 +1,7 @@
 ALTER TABLE invoice_data ADD external_id CHARACTER VARYING;
 ALTER TABLE payment_data ADD external_id CHARACTER VARYING;
 ALTER TABLE refund_data ADD external_id CHARACTER VARYING;
+
+CREATE INDEX IF NOT EXISTS invoice_data_external_id_idx ON mst.invoice_data(external_id);
+CREATE INDEX IF NOT EXISTS payment_data_external_id_idx ON mst.payment_data(external_id);
+CREATE INDEX IF NOT EXISTS refund_data_external_id_idx ON mst.refund_data(external_id);
