955ae5014375b0f3774d12e27a8e50d206f1776f
karleowne <karleowne@gmail.com>
2021-07-24 15:33:09 +0700

    rename annotation

--- src/test/java/com/rbkmoney/magista/config/WithKafkaWithPostgresqlSpringBootITest.java
+++ src/test/java/com/rbkmoney/magista/config/WithKafkaWithPostgresqlSpringBootITest.java
@@ -11,12 +11,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Inherited
-@Documented
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(
         classes = {
