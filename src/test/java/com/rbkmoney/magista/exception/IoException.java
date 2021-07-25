92b6aab016322a5815fc55a3a24fe3397f479c30
karleowne <karleowne@gmail.com>
2021-07-26 02:47:43 +0700

    refactor annotations and test lifecycle

--- src/test/java/com/rbkmoney/magista/exception/IoException.java
+++ src/test/java/com/rbkmoney/magista/exception/IoException.java
@@ -1,23 +1,8 @@
 package com.rbkmoney.magista.exception;
 
 public class IoException extends RuntimeException {
-    public IoException() {
-        super();
-    }
-
-    public IoException(String message) {
-        super(message);
-    }
 
     public IoException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public IoException(Throwable cause) {
-        super(cause);
-    }
-
-    protected IoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
