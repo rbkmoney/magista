5227af4aa17c471a13aacf16876a9ff3f73a4a52
Anatoly Karlov <karleowne@gmail.com>
2021-09-09 22:28:09 +0700

    bump magista-proto, refactor code style like java 15

--- src/test/java/com/rbkmoney/magista/service/MerchantStatisticsServiceTest.java
+++ src/test/java/com/rbkmoney/magista/service/MerchantStatisticsServiceTest.java
@@ -1,7 +1,6 @@
 package com.rbkmoney.magista.service;
 
 import com.rbkmoney.damsel.domain.InvoiceTemplate;
-import com.rbkmoney.geck.common.util.TypeUtil;
 import com.rbkmoney.machinegun.eventsink.MachineEvent;
 import com.rbkmoney.magista.CommonSearchQueryParams;
 import com.rbkmoney.magista.InvoiceTemplateSearchQuery;
@@ -12,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.List;
 
 import static com.rbkmoney.magista.util.InvoiceTemplateGenerator.*;
@@ -27,11 +26,12 @@ public class MerchantStatisticsServiceTest {
     private MerchantStatisticsService merchantStatisticsService;
 
     private InvoiceTemplate invoiceTemplate;
+    private String invoiceTemplateId;
 
     @BeforeEach
     @SneakyThrows
     public void setUp() {
-        String invoiceTemplateId = "invoiceTemplateId";
+        invoiceTemplateId = "invoiceTemplateId";
         invoiceTemplate = getInvoiceTemplate(getCart());
         MachineEvent message = getEvent(
                 invoiceTemplateId,
@@ -41,20 +41,18 @@ public class MerchantStatisticsServiceTest {
     }
 
     @Test
-    public void name() {
+    public void shouldSearchInvoiceTemplates() {
         var commonSearchQueryParams = new CommonSearchQueryParams();
-        commonSearchQueryParams.setFromTime(TypeUtil.temporalToString(LocalDateTime.now().minusDays(1)));
-        commonSearchQueryParams.setToTime(TypeUtil.temporalToString(LocalDateTime.now().plusDays(1)));
+        commonSearchQueryParams.setFromTime(Instant.now().minusSeconds(60).toString());
+        commonSearchQueryParams.setToTime(Instant.now().plusSeconds(60).toString());
         commonSearchQueryParams.setPartyId(invoiceTemplate.getOwnerId());
         commonSearchQueryParams.setShopIds(List.of(invoiceTemplate.getShopId()));
         commonSearchQueryParams.setLimit(1000);
         commonSearchQueryParams.setContinuationToken(null);
         var searchQuery = new InvoiceTemplateSearchQuery();
         searchQuery.setCommonSearchQueryParams(commonSearchQueryParams);
-//        searchQuery.setInvoiceTemplateId(invoiceTemplate.getId());
-//        searchQuery.setInvoiceValidUntil(TypeUtil.temporalToString(getInvoiceValidUntil(
-//                LocalDateTime.now().minusMinutes(1),
-//                invoiceTemplate.getInvoiceLifetime())));
+        searchQuery.setInvoiceTemplateId(invoiceTemplateId);
+        searchQuery.setInvoiceValidUntil(Instant.now().plusSeconds(30).toString());
         searchQuery.setProduct(invoiceTemplate.getProduct());
         var statInvoiceTemplateResponse = merchantStatisticsService.getInvoiceTemplates(searchQuery);
         assertEquals(1, statInvoiceTemplateResponse.getInvoiceTemplates().size());
