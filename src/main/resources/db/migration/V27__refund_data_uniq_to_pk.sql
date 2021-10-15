4119041a2120134d649f9b351852939b6b4a88b2
vitaxa <vitaxa93gamebox@gmail.com>
2021-10-15 17:28:11 +0300

    cleanup

--- src/main/resources/db/migration/V27__refund_data_uniq_to_pk.sql
+++ src/main/resources/db/migration/V27__refund_data_uniq_to_pk.sql
@@ -1,8 +1,6 @@
 CREATE UNIQUE INDEX IF NOT EXISTS refund_data_new_pkey on mst.refund_data (id);
 ALTER TABLE mst.refund_data
     DROP CONSTRAINT IF EXISTS refund_data_pkey;
-ALTER TABLE mst.refund_data
-    ADD PRIMARY KEY USING INDEX refund_data_new_pkey;
 
 DO
 $$
