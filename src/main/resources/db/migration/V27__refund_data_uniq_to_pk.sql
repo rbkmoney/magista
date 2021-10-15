52409a4905e40adefa62388e64722cb2a251be50
vitaxa <vitaxa93gamebox@gmail.com>
2021-10-15 17:27:43 +0300

    migrate refund unique key to primary

---
+++ src/main/resources/db/migration/V27__refund_data_uniq_to_pk.sql
@@ -0,0 +1,18 @@
+CREATE UNIQUE INDEX IF NOT EXISTS refund_data_new_pkey on mst.refund_data (id);
+ALTER TABLE mst.refund_data
+    DROP CONSTRAINT IF EXISTS refund_data_pkey;
+ALTER TABLE mst.refund_data
+    ADD PRIMARY KEY USING INDEX refund_data_new_pkey;
+
+DO
+$$
+    BEGIN
+        IF NOT exists(SELECT constraint_name
+                      FROM information_schema.table_constraints
+                      WHERE table_name = 'refund_data'
+                        AND constraint_type = 'PRIMARY KEY') THEN
+            ALTER TABLE mst.refund_data
+                ADD PRIMARY KEY USING INDEX refund_data_new_pkey;
+        END IF;
+    END
+$$;
