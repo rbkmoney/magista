02e378fc46ccc12211d371847c2df27a95a19cac
vitaxa <vitaxa93gamebox@gmail.com>
2021-10-18 11:43:54 +0300

    moved all index creation to a procedure block

--- src/main/resources/db/migration/V27__refund_data_uniq_to_pk.sql
+++ src/main/resources/db/migration/V27__refund_data_uniq_to_pk.sql
@@ -1,7 +1,3 @@
-CREATE UNIQUE INDEX IF NOT EXISTS refund_data_new_pkey on mst.refund_data (id);
-ALTER TABLE mst.refund_data
-    DROP CONSTRAINT IF EXISTS refund_data_pkey;
-
 DO
 $$
     BEGIN
@@ -9,8 +5,12 @@ $$
                       FROM information_schema.table_constraints
                       WHERE table_name = 'refund_data'
                         AND constraint_type = 'PRIMARY KEY') THEN
+            CREATE UNIQUE INDEX IF NOT EXISTS refund_data_new_pkey on mst.refund_data (id);
+            ALTER TABLE mst.refund_data
+                DROP CONSTRAINT IF EXISTS refund_data_pkey;
             ALTER TABLE mst.refund_data
                 ADD PRIMARY KEY USING INDEX refund_data_new_pkey;
+            ALTER INDEX refund_data_new_pkey RENAME TO refund_data_pkey;
         END IF;
     END
 $$;
