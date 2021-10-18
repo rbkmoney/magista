c63854ec94e0d4aadeda7edef1a92429d2eb0f6c
vitaxa <vitaxa93gamebox@gmail.com>
2021-10-18 12:52:29 +0300

    add schema predicate

--- src/main/resources/db/migration/V27__refund_data_uniq_to_pk.sql
+++ src/main/resources/db/migration/V27__refund_data_uniq_to_pk.sql
@@ -4,6 +4,7 @@ $$
         IF NOT exists(SELECT constraint_name
                       FROM information_schema.table_constraints
                       WHERE table_name = 'refund_data'
+                        AND table_schema = 'mst'
                         AND constraint_type = 'PRIMARY KEY') THEN
             CREATE UNIQUE INDEX IF NOT EXISTS refund_data_new_pkey on mst.refund_data (id);
             ALTER TABLE mst.refund_data
