01428f65ee617005cd8a533c73c850739c89cabf
Anatoly Karlov <a.karlov@rbkmoney.com>
2021-11-25 14:45:20 +0700

    add parse msgpack objects (#341)

--- src/main/java/com/rbkmoney/magista/converter/SourceEventsParser.java
+++ src/main/java/com/rbkmoney/magista/converter/SourceEventsParser.java
@@ -1,24 +1,32 @@
 package com.rbkmoney.magista.converter;
 
-import com.rbkmoney.damsel.payment_processing.EventPayload;
+import com.rbkmoney.damsel.domain.InvoiceTemplate;
+import com.rbkmoney.damsel.payment_processing.*;
 import com.rbkmoney.machinegun.eventsink.MachineEvent;
 import com.rbkmoney.machinegun.msgpack.Value;
 import com.rbkmoney.magista.exception.ParseException;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TUnion;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SourceEventsParser {
 
+    public static final String TPL = "tpl";
+    public static final String ID = "id";
+
+    private final UserInfo userInfo = new UserInfo("magista", UserType.internal_user(new InternalUser()));
+
     private final BinaryConverter<EventPayload> converter;
+    private final InvoiceTemplatingSrv.Iface invoiceTemplatingClient;
 
     public List<EventPayload> parseEvents(MachineEvent message) {
         try {
@@ -26,12 +34,7 @@ public class SourceEventsParser {
                 var bin = message.getData().getBin();
                 return List.of(converter.convert(bin, EventPayload.class));
             } else if (message.getData().isSetArr()) {
-                validateBinaryData(message);
-                return message.getData().getArr().stream()
-                        .filter(Value::isSetBin)
-                        .map(Value::getBin)
-                        .map(bytes -> converter.convert(bytes, EventPayload.class))
-                        .collect(Collectors.toList());
+                return handleArray(message);
             } else {
                 throw new ParseException(String.format("Cant parse type of message=%s", message.getData()));
             }
@@ -41,13 +44,43 @@ public class SourceEventsParser {
         }
     }
 
-    private void validateBinaryData(MachineEvent message) {
-        var errorValues = message.getData().getArr().stream()
-                .filter(value -> !value.isSetBin())
+    private List<EventPayload> handleArray(MachineEvent message) {
+        var thriftArray = message.getData().getArr().stream()
                 .map(TUnion::toString)
                 .collect(Collectors.joining(", "));
-        if (StringUtils.hasText(errorValues)) {
-            throw new ParseException(String.format("Cant parse type of messages=%s", errorValues));
-        }
+        log.warn("MachineEvent contains an ARRAY! instead of a binary, topic typing error, " +
+                "thriftArray={}", thriftArray);
+        return Stream.concat(getBinaries(message), getObjects(message))
+                .collect(Collectors.toList());
+    }
+
+    private Stream<EventPayload> getBinaries(MachineEvent message) {
+        return message.getData().getArr().stream()
+                .filter(Value::isSetBin)
+                .map(Value::getBin)
+                .map(bytes -> converter.convert(bytes, EventPayload.class));
+    }
+
+    private Stream<EventPayload> getObjects(MachineEvent message) {
+        return message.getData().getArr().stream()
+                .filter(Value::isSetObj)
+                .map(value -> value.getObj().get(Value.str(TPL)))
+                .filter(Value::isSetObj)
+                .map(tpl -> tpl.getObj().get(Value.str(ID)))
+                .filter(Value::isSetStr)
+                .map(Value::getStr)
+                .map(this::getInvoiceTemplate)
+                .map(this::getEventPayload);
+    }
+
+    @SneakyThrows
+    private InvoiceTemplate getInvoiceTemplate(String id) {
+        return invoiceTemplatingClient.get(userInfo, id);
+    }
+
+    private EventPayload getEventPayload(InvoiceTemplate invoiceTemplate) {
+        return new EventPayload(
+                EventPayload._Fields.INVOICE_TEMPLATE_CHANGES,
+                List.of(InvoiceTemplateChange.invoice_template_created(new InvoiceTemplateCreated(invoiceTemplate))));
     }
 }
--- src/test/java/com/rbkmoney/magista/listener/InvoiceTemplateServiceTest.java
+++ src/test/java/com/rbkmoney/magista/listener/InvoiceTemplateServiceTest.java
@@ -1,29 +1,36 @@
 package com.rbkmoney.magista.listener;
 
+import com.rbkmoney.damsel.domain.InvoiceTemplate;
 import com.rbkmoney.damsel.payment_processing.EventPayload;
 import com.rbkmoney.damsel.payment_processing.InvoiceTemplateChange;
+import com.rbkmoney.damsel.payment_processing.InvoiceTemplatingSrv;
 import com.rbkmoney.machinegun.eventsink.MachineEvent;
 import com.rbkmoney.machinegun.msgpack.Value;
 import com.rbkmoney.magista.config.PostgresqlSpringBootITest;
+import com.rbkmoney.magista.converter.SourceEventsParser;
 import com.rbkmoney.magista.domain.enums.InvoiceTemplateEventType;
 import com.rbkmoney.magista.exception.NotFoundException;
-import com.rbkmoney.magista.exception.ParseException;
 import com.rbkmoney.magista.service.InvoiceTemplateService;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static com.rbkmoney.magista.domain.tables.InvoiceTemplate.INVOICE_TEMPLATE;
 import static com.rbkmoney.magista.util.InvoiceTemplateGenerator.*;
 import static com.rbkmoney.testcontainers.annotations.util.ThriftUtil.toByteArray;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @PostgresqlSpringBootITest
 public class InvoiceTemplateServiceTest {
@@ -38,6 +45,9 @@ public class InvoiceTemplateServiceTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @MockBean
+    private InvoiceTemplatingSrv.Iface invoiceTemplatingClient;
 
     @Test
     public void shouldHandleAndSave() throws Exception {
@@ -128,10 +138,10 @@ public class InvoiceTemplateServiceTest {
         String invoiceTemplateId = "invoiceTemplateId";
         MachineEvent message = new MachineEvent()
                 .setData(toArray(
-                        getDefaultValue(),
-                        getDefaultValue(),
-                        getDefaultValue(),
-                        getDefaultValue()))
+                        getDefaultInvoiceTemplateValue(),
+                        getDefaultInvoiceTemplateValue(),
+                        getDefaultInvoiceTemplateValue(),
+                        getDefaultInvoiceTemplateValue()))
                 .setCreatedAt(Instant.now().toString())
                 .setEventId(1)
                 .setSourceNs("source_ns")
@@ -146,10 +156,10 @@ public class InvoiceTemplateServiceTest {
         String invoiceTemplateId = "invoiceTemplateId";
         MachineEvent message = new MachineEvent()
                 .setData(toArray(
-                        getDefaultValue(),
+                        getDefaultInvoiceTemplateValue(),
                         toByteArray(EventPayload.invoice_changes(List.of())),
-                        getDefaultValue(),
-                        getDefaultValue()))
+                        getDefaultInvoiceTemplateValue(),
+                        getDefaultInvoiceTemplateValue()))
                 .setCreatedAt(Instant.now().toString())
                 .setEventId(1)
                 .setSourceNs("source_ns")
@@ -161,21 +171,30 @@ public class InvoiceTemplateServiceTest {
 
     @Test
     public void shouldNotParseArrayEvents() throws Exception {
+        String repairId = "repairId";
+        InvoiceTemplate invoiceTemplate = getInvoiceTemplate(getCart());
+        when(invoiceTemplatingClient.get(any(), eq(repairId))).thenReturn(invoiceTemplate);
         String invoiceTemplateId = "invoiceTemplateId";
         MachineEvent message = new MachineEvent()
                 .setData(toArray(
-                        getDefaultValue(),
+                        getDefaultInvoiceTemplateValue(),
+                        // мы не можем выкидывать исключение в SourceEventsParser из за неправильной типизации,
+                        // поэтому будем такие значения скипать
                         Value.str("error"),
-                        getDefaultValue(),
-                        getDefaultValue()))
+                        Value.i(3),
+                        // подобный вид msgpack будем обрабатывать
+                        Value.obj(Map.of(
+                                Value.str(SourceEventsParser.TPL),
+                                Value.obj(Map.of(Value.str(SourceEventsParser.ID), Value.str(repairId)))))))
                 .setCreatedAt(Instant.now().toString())
                 .setEventId(1)
                 .setSourceNs("source_ns")
                 .setSourceId(invoiceTemplateId);
-        assertThrows(ParseException.class, () -> invoiceTemplateListener.handleMessages(List.of(message)));
+        invoiceTemplateListener.handleMessages(List.of(message));
+        verify(invoiceTemplatingClient, timeout(5000).times(1)).get(any(), eq(repairId));
     }
 
-    private Value getDefaultValue() {
+    private Value getDefaultInvoiceTemplateValue() {
         return toByteArray(EventPayload.invoice_template_changes(List.of(getCreated(getInvoiceTemplate(getCart())))));
     }
 
