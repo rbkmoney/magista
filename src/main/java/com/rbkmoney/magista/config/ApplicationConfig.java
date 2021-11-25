74866a5b6da0e4ea7c0fc18882a8830848d635fd
Anatoly Karlov <karleowne@gmail.com>
2021-11-25 13:37:52 +0700

    add parse msgpack objects

--- src/main/java/com/rbkmoney/magista/config/ApplicationConfig.java
+++ src/main/java/com/rbkmoney/magista/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.rbkmoney.magista.config;
 
+import com.rbkmoney.damsel.payment_processing.InvoiceTemplatingSrv;
 import com.rbkmoney.damsel.payment_processing.PartyManagementSrv;
 import com.rbkmoney.woody.thrift.impl.http.THSpawnClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,6 +20,18 @@ public class ApplicationConfig {
     ) throws IOException {
         return new THSpawnClientBuilder()
                 .withNetworkTimeout(networkTimeout)
-                .withAddress(resource.getURI()).build(PartyManagementSrv.Iface.class);
+                .withAddress(resource.getURI())
+                .build(PartyManagementSrv.Iface.class);
+    }
+
+    @Bean
+    public InvoiceTemplatingSrv.Iface invoiceTemplatingClient(
+            @Value("${service.invoiceTemplating.url}") Resource resource,
+            @Value("${service.invoiceTemplating.networkTimeout}") int networkTimeout)
+            throws IOException {
+        return new THSpawnClientBuilder()
+                .withNetworkTimeout(networkTimeout)
+                .withAddress(resource.getURI())
+                .build(InvoiceTemplatingSrv.Iface.class);
     }
 }
